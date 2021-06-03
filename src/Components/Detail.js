69770cc65b3d65a4bf3b3b0a469fa85e057d10dd
dsimp <55302995+dsimp@users.noreply.github.com>
2021-06-03 12:29:44 -0500

    detail pic backround - spacing

---
+++ src/Components/Detail.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import useFetchId from "./services/useFetchId.js";
+import Pic from "./Pic.js";
+import LogoNav from "./LogoNav";
+import "./Detail.css";
+// eslint-disable-next-line react/prop-types
+const Detail = ({ match }) => {
+  const { details } = useFetchId(match.params.id);
+  const mix = [];
+
+  for (let i = 1; i < 16; i++) {
+    if (details[`strMeasure${i}`] == null || details[`strMeasure${i}`] == "") {
+      break;
+    }
+    mix.push(
+      <Pic
+        key={details[`strIngredient${i}`]}
+        ingredient={details[`strIngredient${i}`]}
+        measure={details[`strMeasure${i}`]}
+      />
+    );
+  }
+
+  let cTail = details.strDrink;
+  document.title = `${cTail} Detail Page`;
+
+  return (
+    <Fragment>
+      <LogoNav />
+
+      <div className="dt">
+        <h1> {details.strDrink} </h1>
+        <h1> {details.strGlass}</h1>
+
+        <div className="mix">{mix}</div>
+        <div>
+          <h1>{details.strInstructions}</h1>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Detail;
