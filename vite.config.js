58017f38b425a3cab8163e9652d4c6938c520dbb
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2026-02-10 13:44:33 -0600

    Add Netlify configuration

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,4 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react/jsx-runtime', 'react-dom/client', 'react-redux'],
-  },
-  resolve: {
-    alias: {
-      'react/jsx-runtime': path.resolve(__dirname, 'node_modules/react/jsx-runtime.js'),
-      'react-dom/client': path.resolve(__dirname, 'node_modules/react-dom/client.js'),
-      'react-redux': path.resolve(__dirname, 'node_modules/react-redux/dist/react-redux.mjs'),
-    },
-  },
 });
