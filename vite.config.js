117bd245a83c36f5e46522c20b587332c493514f
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2026-02-10 12:54:44 -0600

    Add explicit alias for react-redux and include in optimizeDeps

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,13 @@ export default defineConfig({
     outDir: 'build',
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react/jsx-runtime', 'react-dom/client'],
+    include: ['react', 'react-dom', 'react/jsx-runtime', 'react-dom/client', 'react-redux'],
   },
   resolve: {
     alias: {
       'react/jsx-runtime': path.resolve(__dirname, 'node_modules/react/jsx-runtime.js'),
       'react-dom/client': path.resolve(__dirname, 'node_modules/react-dom/client.js'),
+      'react-redux': path.resolve(__dirname, 'node_modules/react-redux/dist/react-redux.mjs'),
     },
   },
 });
