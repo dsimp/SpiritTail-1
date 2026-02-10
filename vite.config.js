1bd12e0d4fbb568e2b84bf9e6947733dac872b03
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2026-02-10 12:42:24 -0600

    Remove classic JSX runtime in favor of defaults

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'classic',
-  })],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
